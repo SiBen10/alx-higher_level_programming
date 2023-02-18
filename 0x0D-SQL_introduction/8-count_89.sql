de9201f57b47eedcf3c97044b96347711a50ea82
SiBen10 <benjaminkn805@gmail.com>
2023-02-17 16:18:49 -0800

    introduction to SQL

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,7 @@
+-- Displays the number of records with id = 89 in the table first_table in my MySQL server.
+SELECT COUNT(*)
+FROM `first_table`
+WHERE `id` = 89;-- Displays the number of records with id = 89 in the table first_table in my MySQL server.
+SELECT COUNT(*)
+FROM `first_table`
+WHERE `id` = 89;
---
+++ 0x0D-SQL_introduction/README.md
@@ -0,0 +1,4 @@
+# SQL - Introduction
+
+
+In this project, we began working on relational databases. I started practising introductory SQL data definitions and data manipulation language, making subqueries, and using functions
