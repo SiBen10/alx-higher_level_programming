c563ac4e3489f904e1826e796e34ebd6f20fb9c9
SiBen10 <benjaminkn805@gmail.com>
2023-03-13 19:35:59 -0700

    Java Warm up

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  console.log('Not a number');
+} else {
+  console.log('My number:', parseInt(process.argv[2]));
+}
+
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,6 @@
+# 0x12. Javascript - Warm up
+
+## Description
+
+
+The aim of this project is to get started with Javascript, learn all the syntax and its different uses.
